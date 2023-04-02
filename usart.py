762ef202d47911896cddae6c9380e3f65acfab2e
ceekinbox <1040701718@qq.com>
2023-04-02 21:18:17 +0800

    update def SolveTranslate(src).

--- usart.py
+++ usart.py
@@ -61,12 +61,51 @@ class serialMain:
 #    def serial_creat_thread(self):
 #        thread_read = threading.Thread(target=self.serial_read, daemon=True)
 #        thread_read.start()
-       # print('thread created')
+# print('thread created')
+
+
+# 下面实现了一个简易的字符分析器，可将F F’ F2 转换成单个字母
+# 调用SolveTrainslate 参数为源字符串
+# 注意！ 源字符串必须为kociemba.solve()输出的，每步之间有空格分隔的格式，否则输出为空！！
+# 注意！ 源字符串必须为kociemba.solve()输出的，每步之间有空格分隔的格式，否则输出为空！！
+# 注意！ 源字符串必须为kociemba.solve()输出的，每步之间有空格分隔的格式，否则输出为空！！
+
+theTransDict = {"F'": "Q", "F2": "W", "F": "F",
+                "L'": "E", "L2": "T", "L": "L",
+                "U'": "Y", "U2": "I", "U": "U",
+                "D'": "O", "D2": "P", "D": "D",
+                "B'": "A", "B2": "S", "B": "B",
+                "R'": "G", "R2": "H", "R": "R"}
+def isLetter(src):
+    return src.isalpha()
+
+def isNumber(src):
+    if src == "'":
+        return True
+    else:
+        return src.isdigit()
+
+def isSpace(src):
+    return src == ' '
+def SolveTranslate(src):
+    dst = ""
+    temp = ""
+    src += " "
+    for s in src:
+        if isLetter(s):
+            temp += s
+        elif isSpace(s):
+            if temp != "":
+                dst += theTransDict[temp]
+                temp = ""
+            continue
+        elif isNumber(s):
+            temp += s
+    return dst
+
+
 if __name__ == "__main__":
     a = 0
-    x = kociemba.solve('DRLUUBFBRBLURRLRUBLRDDFDLFUFUFFDBRDUBRUFLLFDDBFLUBLRBD')
-    print(type(x))
-    print(x)
 #   example:
 #   usart1 = serialMain()
 #   usart1.serial_init("COM1")
